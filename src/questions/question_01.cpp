8cefc6b35d74e48967d11ec1116b2e645d757e77
DaytonPlus <101413618+DaytonPlus@users.noreply.github.com>
2023-03-11 18:23:18 -0500

    Create question_01.cpp

---
+++ src/questions/question_01.cpp
@@ -0,0 +1,8 @@
+Problem getProblem1() {
+ Problem problem1;
+ problem1.setId(1);
+ problem1.setName("Numero Perfecto");
+ problem1.setDescription("Si el numero es Igual a la suma de sus divisores positivos.");
+ problem1.setData("Escriba un programa que lea un numero entero positivo n y determine si es un numero perfecto.");
+ return problem1;
+}
