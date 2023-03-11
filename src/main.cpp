e100088f4f77748ff58083532ff1cffb6de1889b
DaytonPlus <101413618+DaytonPlus@users.noreply.github.com>
2023-03-11 18:20:28 -0500

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+#include "includes/KeySign.cpp"
+#include "includes/Menu.cpp"
+
+int main()
+{
+	int key = keySign();
+	Menu menu(key);
+	menu.init();
+	return 0;
+}
